e971e1ad00fa34d1ace7fe5b6b22e09b10740bbb
phucq.ins <greenlogitics@gmail.com>
2021-10-21 08:33:26 +0700

    first-commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,104 @@
+import { React, useRef, useEffect } from "react";
+import logo from "../assets/images/Logo-2.png";
+import { Link, useLocation } from "react-router-dom";
+
+const mainNav = [
+  {
+    display: "Trang chủ",
+    path: "/",
+  },
+  {
+    display: "Sản phẩm",
+    path: "/catalog",
+  },
+  {
+    display: "Phụ kiện",
+    path: "/accessories",
+  },
+  {
+    display: "Liên hệ",
+    path: "/contact",
+  },
+];
+
+const Header = () => {
+  const { pathname } = useLocation();
+  const activeNav = mainNav.findIndex((e) => e.path === pathname);
+  const headerRef = useRef(null);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (
+        document.body.scrollTop > 80 ||
+        document.documentElement.scrollTop > 80
+      ) {
+        headerRef.current.classList.add("shrink");
+      } else {
+        headerRef.current.classList.remove("shrink");
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll", "");
+    };
+  }, []);
+
+  const menuLeft = useRef(null);
+  const menuToggle = () => {
+    menuLeft.current.classList.toggle("active");
+  };
+
+  return (
+    <div className="header" ref={headerRef}>
+      <div className="container">
+        <div className="header__logo">
+          <Link to="/">
+            <img src={logo} alt=""/>
+          </Link>
+        </div>
+        <div className="header__menu"  >
+          <div className="header__menu__mobile-toggle" onClick={menuToggle}>
+            <i className="bx bx-menu-alt-left"></i>
+          </div>
+          <div className="header__menu__left" ref={menuLeft} >
+            <div className="header__menu__left__close" onClick={menuToggle}>
+              <i className="bx bx-chevron-left"></i>
+            </div>
+
+            {mainNav.map((ele, i) => (
+              <div
+                onClick={menuToggle}
+                key={i}
+                className={`header__menu__item header__menu__left__item ${
+                  i === activeNav ? "active" : ""
+                }`}
+              >
+                <Link to={ele.path}>
+                  <span>{ele.display}</span>
+                </Link>
+              </div>
+            ))}
+          </div>
+          <div className="header__menu__right">
+            <div className="header__menu__item header__menu__right__item">
+              <Link>
+                <i className="bx bx-search"></i>
+              </Link>
+            </div>
+            <div className="header__menu__item header__menu__right__item">
+              <Link to="/cart">
+                <i className="bx bx-shopping-bag"></i>
+              </Link>
+            </div>
+            <div className="header__menu__item header__menu__right__item" onClick={menuToggle}>
+              <Link>
+                <i className="bx bx-user"></i>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
