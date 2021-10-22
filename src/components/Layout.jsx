7ae57a9a3ac24bc2421b57f3329d0c0825cb4a71
phucq.ins <greenlogitics@gmail.com>
2021-10-22 10:19:54 +0700

    fix react-router-dom on gh-pages

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,28 +1,26 @@
-import React from 'react'
+import React from "react";
 
-import { BrowserRouter, Route } from 'react-router-dom'
+import { HashRouter, Route } from "react-router-dom";
 
-import Header from './Header'
-import Footer from './Footer'
+import Header from "./Header";
+import Footer from "./Footer";
 
-import Routes from '../routes/Routes'
+import Routes from "../routes/Routes";
 
 const Layout = () => {
-    return (
-        <BrowserRouter>
-            <Route render={props => (
-                <div>
-                    <Header {...props}/>
-                    <div className="container">
-                        <div className="main">
-                            <Routes/>
-                        </div>
-                    </div>
-                    <Footer/>
-                </div>
-            )}/>
-        </BrowserRouter>
-    )
-}
+  return (
+    <HashRouter>
+      <div>
+        <Header />
+        <div className="container">
+          <div className="main">
+            <Routes />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </HashRouter>
+  );
+};
 
-export default Layout
+export default Layout;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
+import React from "react";
 
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch } from "react-router-dom";
 
-import Home from '../pages/Home'
-import Catalog from '../pages/Catalog'
-import Cart from '../pages/Cart'
-import Products from '../pages/Products'
+import Home from "../pages/Home";
+import Catalog from "../pages/Catalog";
+import Cart from "../pages/Cart";
+import Products from "../pages/Products";
 
 const Routes = () => {
-    return (
-        <Switch>
-            <Route path='/' exact component={Home}/>
-            <Route path='/catalog/:slug' component={Products}/>
-            <Route path='/catalog' component={Catalog}/>
-            <Route path='/cart' component={Cart}/>
-        </Switch>
-    )
-}
+  return (
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/catalog/:slug" component={Products} />
+      <Route path="/catalog" component={Catalog} />
+      <Route path="/cart" component={Cart} />
+    </Switch>
+  );
+};
 
-export default Routes
+export default Routes;
